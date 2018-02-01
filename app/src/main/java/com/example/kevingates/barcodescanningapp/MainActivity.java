49154264744ac5e4830662a80016badecc6bd90b
Kevin Gates <kevinobamatheus@gmail.com>
2018-02-01 17:09:17 +0800

    upload

--- app/src/main/java/com/example/kevingates/barcodescanningapp/MainActivity.java
+++ app/src/main/java/com/example/kevingates/barcodescanningapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -26,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         integrator.setPrompt("Scan");
         integrator.setCameraId(0);
         integrator.setBeepEnabled(false);
-        //integrator.setBarcodeImageEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
         integrator.initiateScan();
     }
 
@@ -35,11 +36,15 @@ public class MainActivity extends AppCompatActivity {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if(result != null) {
             if(result.getContents() == null) {
-                Log.d("MainActivity", "Cancelled scan");
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                //Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled scan", Toast.LENGTH_LONG).show();
+
             } else {
-                Log.d("MainActivity", "Scanned");
+                //Log.d("MainActivity", "Scanned");
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+
+                TextView resultContent = (TextView) findViewById(R.id.resultContent);
+                resultContent.setText(result.getContents()); //set text for text view
             }
         } else {
             // This is important, otherwise the result will not be passed to the fragment
